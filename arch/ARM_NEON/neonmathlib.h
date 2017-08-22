0719fca7f2b6b6e3c05491cc8bf0a24d79345851
Biagio Cosenza <cosenza@tu-berlin.de>
2017-08-22 19:22:41 +0200

    working on NEON suport

---
+++ arch/ARM_NEON/neonmathlib.h
@@ -0,0 +1,18 @@
+#ifndef __NEON_MATH_LIB_H__
+#define __NEON_MATH_LIB_H__
+
+#include "arm_neon.h"
+
+
+#ifdef GCC
+#define ALIGN(A) __attribute__ ((aligned (A))
+#else
+#define ALIGN(A)
+#endif
+
+
+
+
+
+
+#endif
